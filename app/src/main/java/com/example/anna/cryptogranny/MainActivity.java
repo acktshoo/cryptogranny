50d096733c2ddb267a3ef9d49ba4887ebd74901d
Anna Komarova <acktshoo@gmail.com>
2017-10-10 23:27:56 -0700

    adding puzzle as set of buttons

--- app/src/main/java/com/example/anna/cryptogranny/MainActivity.java
+++ app/src/main/java/com/example/anna/cryptogranny/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView puzzleView;
     private TextView curSolView;
     private Cryptogranny cryptogranny;
-    private Character fromM = 'P';
+    private Character fromM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +42,42 @@ public class MainActivity extends AppCompatActivity {
 
         cryptogranny = new Cryptogranny("PUZZLE FOO");
         puzzleView.setText(cryptogranny.getPuzzle());
-        
+
+        LinearLayout puzzleLayout = (LinearLayout) findViewById(R.id.puzzleLayout);
+        for(Character m: cryptogranny.getPuzzle().toCharArray()){
+            Button puzzleButton = new Button(this);
+            puzzleButton.setText(m.toString());
+            puzzleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Button button = (Button) view;
+                    handleClickOnN(button.getText().charAt(0));
+                }
+            });
+            puzzleLayout.addView(puzzleButton);
+        }
+
         updatePuzzleState();
+    }
+
+    private void handleClickOnN(Character fromM) {
+        this.fromM = fromM;
     }
 
     private void handleClickClear() {
-        cryptogranny.clearM(fromM);
-        updatePuzzleState();
+        if(fromM != null) {
+            cryptogranny.clearM(fromM);
+            fromM = null;
+            updatePuzzleState();
+        }
     }
 
     private void handleClickOnM(Character toN) {
-        cryptogranny.guess(fromM,toN);
-        updatePuzzleState();
+        if(fromM != null) {
+            cryptogranny.guess(fromM, toN);
+            fromM = null;
+            updatePuzzleState();
+        }
     }
 
     private void updatePuzzleState() {
