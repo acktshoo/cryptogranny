79fb9dc9d95772ba41889623efc5ea1c544c4693
Anna Komarova <acktshoo@gmail.com>
2017-11-05 17:23:35 -0800

    Added launch screen

--- app/src/main/java/com/example/anna/cryptogranny/MainActivity.java
+++ app/src/main/java/com/example/anna/cryptogranny/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.anna.cryptogranny;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_puzzle);
+        setContentView(R.layout.activity_main);
+
+        Button newPuzzleButton = (Button) findViewById(R.id.newPuzzle);
+        newPuzzleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleNewPuzzle();
+            }
+        });
+    }
+
+    private void handleNewPuzzle() {
+        Intent intent = new Intent(this, PuzzleActivity.class );
+        startActivity(intent);
     }
 
 }
