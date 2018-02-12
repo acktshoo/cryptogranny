d1ca6eb47993bd56c716512895c0cddabe0ff2d2
Anna Komarova <acktshoo@gmail.com>
2018-02-11 19:27:25 -0800

    added up button to toolbar of input activity

--- app/src/main/java/com/example/anna/cryptogranny/InputActivity.java
+++ app/src/main/java/com/example/anna/cryptogranny/InputActivity.java
@@ -21,6 +21,7 @@ public class InputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button startPuzzleButton = (Button) findViewById(R.id.startPuzzle);
         startPuzzleButton.setOnClickListener(new View.OnClickListener() {
